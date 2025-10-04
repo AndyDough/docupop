87b235ece7b13dd6e8fda7c7d00a0c1c5279f758
Deglakek <hayden.e.kelley@gmail.com>
2025-10-04 15:09:44 -0700

    Added Log out button

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -4,12 +4,21 @@ import {
 	NavigationMenuItem,
 	NavigationMenuList,
 } from "@/components/ui/navigation-menu";
+import { Button } from "@/components/ui/button";
 
 export default function NavMenu() {
+	function handleLogout() {
+		// TODO: Implement actual logout logic (e.g., signOut from next-auth)
+		console.log("Logout clicked");
+	}
+
 	return (
 		<NavigationMenu>
 			<NavigationMenuList>
 				<NavigationMenuItem>
+					<Button variant="outline" onClick={handleLogout}>
+						Logout
+					</Button>
 				</NavigationMenuItem>
 			</NavigationMenuList>
 		</NavigationMenu>
